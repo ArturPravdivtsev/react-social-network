33f468bf068fde7d83087f469219e94fcea0e323
ArturPravdivtsev <artur.pravdivtsev@gmail.com>
2019-08-17 13:39:19 +0300

    Init
    Init commit with some basic Components.

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className="header">
+                <img src="https://i.ytimg.com/vi/tRutb-YH3qg/maxresdefault.jpg"/>
+            </header>;
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className="content">
+                <div><img src="https://i.ytimg.com/vi/hHUWhW9zK3M/hqdefault.jpg" /> </div>
+                <div><img src="https://ctl.s6img.com/society6/img/rPcqOyy-DQYPqnjvwqrYn2zJYgg/w_700/prints/~artwork/s6-original-art-uploads/society6/uploads/misc/3229d5e764b4446ea2d41b89b28e9182/~~/moraine-lake-landscape-photography-prints.jpg" /></div>
+                <div>
+                    My Posts
+                    <div>New Post</div>
+                    <div>
+                        Posts
+                        <div>Post1</div>
+                        <div>Post2</div>
+                    </div>
+                </div>
+            </div>;
+}
+
+export default Profile;
