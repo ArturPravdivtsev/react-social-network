fa05e444f5363b4d56c0378d703285f7e60fb0d6
ArturPravdivtsev <artur.pravdivtsev@gmail.com>
2019-08-20 20:32:55 +0300

    CSS Modules added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
-    return <header className="header">
+    return <header className={s.header}>
                 <img src="https://i.ytimg.com/vi/tRutb-YH3qg/maxresdefault.jpg"/>
             </header>;
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
-    return <div className="content">
+    return <div className={s.content}>
                 <div><img src="https://i.ytimg.com/vi/hHUWhW9zK3M/hqdefault.jpg" /> </div>
                 <div><img src="https://ctl.s6img.com/society6/img/rPcqOyy-DQYPqnjvwqrYn2zJYgg/w_700/prints/~artwork/s6-original-art-uploads/society6/uploads/misc/3229d5e764b4446ea2d41b89b28e9182/~~/moraine-lake-landscape-photography-prints.jpg" /></div>
                 <div>
                     My Posts
                     <div>New Post</div>
-                    <div>
+                    <div className={s.posts}>
                         Posts
-                        <div>Post1</div>
-                        <div>Post2</div>
+                        <div className={s.item}>Post1</div>
+                        <div className={s.item}>Post2</div>
                     </div>
                 </div>
             </div>;
