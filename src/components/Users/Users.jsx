c8e83cf71736d58908ba2e00733bfd9857889845
ArturPravdivtsev <artur.pravdivtsev@gmail.com>
2019-09-21 18:02:07 +0300

    SamuraiJs API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,49 +1,21 @@
 import React from 'react';
 import s from './Users.module.css';
+import Axios from 'axios';
+import userPhoto from '../../assets/images/ava.png';
 
 let Users = (props) => {
     if(props.users.length === 0) {
-    props.setUsers([
-        {
-            id: 1, 
-            photoUrl: 'https://avatars.mds.yandex.net/get-pdb/25978/7185b4a6-13b9-4f7e-a537-9a474db4967b/s1200', 
-            followed: false, 
-            fullname: 'Dmitry', 
-            status: 'I am a boss', 
-            location: {
-                city: 'Minsk', 
-                country: 'Belarus'
-            }
-        },
-        {
-            id: 2, 
-            photoUrl: 'https://avatars.mds.yandex.net/get-pdb/25978/7185b4a6-13b9-4f7e-a537-9a474db4967b/s1200', 
-            followed: true, 
-            fullname: 'Sasha', 
-            status: 'I am a boss too', 
-            location: {
-                city: 'Moscow', 
-                country: 'Rusia'
-            }
-        },
-        {
-            id: 3, 
-            photoUrl: 'https://avatars.mds.yandex.net/get-pdb/25978/7185b4a6-13b9-4f7e-a537-9a474db4967b/s1200', 
-            followed: false, 
-            fullname: 'Andrew', 
-            status: 'I am a boss too', 
-            location: {
-                city: 'Kiev',
-                country: 'Ukraine'
-            }
-        },
-    ]);
+        Axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                debugger;
+                props.setUsers(response.data.items);
+            });
     }
     return <div>
         {props.users.map(u => <div key={u.id} >
             <span>
                 <div>
-                    <img src={u.photoUrl} className={s.userPhoto} />
+                    <img src={ u.photos.small != null ? u.photos.small : userPhoto } className={s.userPhoto} />
                 </div>
                 <div>
                     { u.followed 
@@ -54,12 +26,12 @@ let Users = (props) => {
             </span>
             <span>
                 <span>
-                    <div>{u.fullname}</div>
+                    <div>{u.name}</div>
                     <div>{u.status}</div>
                 </span>
                 <span>
-                    <div>{u.location.country}</div>
-                    <div>{u.location.city}</div>
+                    <div>{"u.location.country"}</div>
+                    <div>{"u.location.city"}</div>
                 </span>
             </span>
             </div>
