459c5e8728f8fde4991afc5e4baac317a2a2bad0
ArturPravdivtsev <artur.pravdivtsev@gmail.com>
2019-10-04 22:22:50 +0300

    ProfileContainer, With Router

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ import s from './Profile.module.css';
 
 const Profile = (props) => {
     return <div>
-                <ProfileInfo />
+                <ProfileInfo profile={props.profile} />
                 <MyPostsContainer />
             </div>;
 }
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
+import Preloader from '../../common/Preloader/Preloader';
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+    if(!props.profile) {
+        return <Preloader />
+    }
+
     return <div>
                 <div>
                     <img src="https://i.ytimg.com/vi/hHUWhW9zK3M/hqdefault.jpg" />
                 </div>
                 <div className={s.descriptionBlock}>
-                    <img src="https://ctl.s6img.com/society6/img/rPcqOyy-DQYPqnjvwqrYn2zJYgg/w_700/prints/~artwork/s6-original-art-uploads/society6/uploads/misc/3229d5e764b4446ea2d41b89b28e9182/~~/moraine-lake-landscape-photography-prints.jpg" />
+                    <img src={props.profile.photos.large} />
                 </div>
             </div>;
 }
